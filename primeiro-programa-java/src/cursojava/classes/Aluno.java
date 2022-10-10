6506b4f53f5d6f9cb600bf00bc0fd0e9ddf2ed4a
michellerdias <michelle.rendias@gmail.com>
2022-10-10 10:35:09 -0300

    Até aula 9.10

---
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,119 @@
+package cursojava.classes;
+
+//Esta é nossa classe/objeto que representa o aluno
+public class Aluno {
+	
+	//Esses são os atributos do Aluno:
+	public String nome;
+	public int idade;
+	String dataNascimento;
+	String registroGeral; //RG
+	String cpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+	public Aluno() { //Cria os dados na memória - sendo padrão do Java
+	//Quando dá um "new", ele chama esse construtor
+	//
+	}
+	
+	public Aluno(String nomePadrao) { 
+		nome = nomePadrao;
+	}
+	
+	public Aluno (String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+	
+	//Veremos os métodos SETTERS e GETTERS do objeto
+	//SET é para adicionar ou receber dados para os atributos
+	//GET é para resgatar ou obter o valor do atributo
+	
+	//Recebe dados
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	
+	
+	
+}
---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,34 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//new Aluno() é uma instância (Criação de Objeto)
+		Aluno aluno1 = new Aluno(); //Agora temos um objeto real na memória
+		aluno1.setNome("João da Silva");
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("01/12/1994");
+		aluno1.setRegistroGeral("17447231");
+		aluno1.setCpf("119.855.784-48");
+		aluno1.setNomeMae("Maria");
+		aluno1.setNomePai("Pedro");
+		aluno1.setDataMatricula("01/01/2000");
+		aluno1.setSerieMatriculado("5a série");
+		aluno1.setNomeEscola("Escola JDev Treinamento");
+		
+		System.out.println("Nome é = " + aluno1.getNome());
+		System.out.println("Idade = " + aluno1.getIdade());
+		System.out.println("Data de nascimento = " + aluno1.getDataNascimento());
+		Aluno aluno2 = new Aluno(); 
+		
+		Aluno aluno3 = new Aluno("Maria"); //Esse objeto sempre terá como nomePadrao Maria
+		
+		Aluno aluno4 = new Aluno("José", 50);
+		
+	}
+
+}
