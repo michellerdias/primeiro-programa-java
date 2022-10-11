07e204bcec22793e3f3b6e520e7ac85790e59589
Michelle Dias <111688509+michellerdias@users.noreply.github.com>
2022-10-11 08:19:36 -0300

    Update PrimeiraClasseJava.java

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -7,9 +7,9 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//new Aluno() È uma inst‚ncia (CriaÁ„o de Objeto)
-		Aluno aluno1 = new Aluno(); //Agora temos um objeto real na memÛria
-		aluno1.setNome("Jo„o da Silva");
+		//new Aluno() √© uma inst√¢ncia (Cria√ß√£o de Objeto)
+		Aluno aluno1 = new Aluno(); //Agora temos um objeto real na mem√≥ria
+		aluno1.setNome("Jo√£o da Silva");
 		aluno1.setIdade(50);
 		aluno1.setDataNascimento("01/12/1994");
 		aluno1.setRegistroGeral("17447231");
@@ -17,17 +17,19 @@ public class PrimeiraClasseJava {
 		aluno1.setNomeMae("Maria");
 		aluno1.setNomePai("Pedro");
 		aluno1.setDataMatricula("01/01/2000");
-		aluno1.setSerieMatriculado("5a sÈrie");
+		aluno1.setSerieMatriculado("5a s√©rie");
 		aluno1.setNomeEscola("Escola JDev Treinamento");
 		
-		System.out.println("Nome È = " + aluno1.getNome());
+		System.out.println("Nome √© = " + aluno1.getNome());
 		System.out.println("Idade = " + aluno1.getIdade());
 		System.out.println("Data de nascimento = " + aluno1.getDataNascimento());
+		System.out.println("Idade = " + aluno1.getIdade());
+		
 		Aluno aluno2 = new Aluno(); 
 		
-		Aluno aluno3 = new Aluno("Maria"); //Esse objeto sempre ter· como nomePadrao Maria
+		Aluno aluno3 = new Aluno("Maria"); //Esse objeto sempre ter√° como nomePadrao Maria
 		
-		Aluno aluno4 = new Aluno("JosÈ", 50);
+		Aluno aluno4 = new Aluno("Jos√©", 50);
 		
 	}
 
