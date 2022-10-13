d2df485417ac0d55b0a7895b26a9a6ddb3830c51
Michelle Dias <111688509+michellerdias@users.noreply.github.com>
2022-10-13 20:20:54 -0300

    Update Aluno.java
    Até aula 9.15

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -174,7 +174,40 @@ public class Aluno {
 			return "Aluno Reprovado";
 		}
 	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
+				+ registroGeral + ", cpf=" + cpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai + ", dataMatricula="
+				+ dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado=" + serieMatriculado + ", nota1="
+				+ nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4 + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
-
